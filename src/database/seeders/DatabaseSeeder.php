2b97379c5e47768dcdb6a0e227b1374fcfb27b53
haruka ishii <harukaishii.99@gmail.com>
2025-06-13 22:26:25 +0900

    databaseseederの修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,8 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(CategoriesTableSeeder::class);
-         //\App\Models\User::factory(10)->create();
+        $this->call(CategoriesTableSeeder::class);
+         \App\Models\User::factory(10)->create();
         $this->call(ContactsTableSeeder::class);
     }
 }
+
